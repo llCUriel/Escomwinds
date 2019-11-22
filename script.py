17ffe21015c6e8fcaa90e14dcdeef7ec380b25ce
CESAR URIEL <uuriel12009u@gmail.com>
2019-11-21 18:59:27 -0600

    Josue

--- script.py
+++ script.py
@@ -2,5 +2,6 @@ import subprocess
 
 
 subprocess.run('git add .', shell=True)
-subprocess.run('echo "llCUriel" | git commit -m "Josue"', shell=True)
+subprocess.run('echo "58542376" | sudo -S git commit -m "Josue"', shell=True)
+subprocess.run('echo "uuriel12009u@gmail.com"')
 subprocess.run('git push origin master', shell=True)
