d9e48eab6bc2acfb350e2d89ee04709426ba9949
CESAR URIEL <uuriel12009u@gmail.com>
2019-11-24 02:39:13 -0600

    Josue

--- App.py
+++ App.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from flask import render_template
+from netmiko import ConnectHandler
+import subprocess
+import threading
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('dashboard.html')
 
+@app.route("/routerCentral")
+def routerCentral():
+    t = threading.Thread(target=conectarARouter, args=('10.168.100.22',))
+    t.start()
+    return render_template('dashboard.html')
+
+def conectarARouter(ip):
+    command = "telnet 10.168.100.22"
+    subprocess.call(command, shell=True)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8001, debug=True)
