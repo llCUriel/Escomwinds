c9c7d33159645b8aef6b6f80531454feba95c00c
CESAR URIEL <uuriel12009u@gmail.com>
2019-11-25 02:41:08 -0600

    Josue

--- App.py
+++ App.py
@@ -3,7 +3,12 @@ from flask import render_template
 from netmiko import ConnectHandler
 import subprocess
 import threading
-
+import random
+import time
+from flask import redirect
+from flask import url_for
+import json
+from flask import Response
 
 app = Flask(__name__)
 
@@ -13,14 +18,19 @@ def index():
 
 @app.route("/routerCentral")
 def routerCentral():
-    t = threading.Thread(target=conectarARouter, args=('10.168.100.22',))
-    t.start()
-    return render_template('dashboard.html')
+    listaDeTemperaturas = generarTemperatura(40,100)
+    return render_template('dashboard.html', listaDeTemperaturas = listaDeTemperaturas)
 
 def conectarARouter(ip):
     command = "telnet 10.168.100.22"
     subprocess.call(command, shell=True)
 
+
+def generarTemperatura(min,max):
+    listaDeTemperaturas = []
+    temperature = random.randrange(min,max)
+    listaDeTemperaturas.append(temperature)
+    return listaDeTemperaturas
 
 if __name__ == "__main__":
     app.run(port=8001, debug=True)
