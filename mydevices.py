1c40981602464a4be6349b71b77e3d182de49321
llCUriel <48939025+llCUriel@users.noreply.github.com>
2019-12-02 23:45:21 -0600

    Update mydevices.py

--- mydevices.py
+++ mydevices.py
@@ -4,3 +4,46 @@ cisco_ios_centralrouter = {
     'username': 'cisco',
     'password': 'cisco'
 }
+
+
+cisco_ios_developDepartamentRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.10.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+cisco_ios_serversRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.20.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+cisco_ios_networksRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.30.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+cisco_ios_networksRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.40.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+cisco_ios_financesRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.40.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
+
+cisco_ios_HRRouter = {
+    'device_type': 'cisco_ios',
+    'ip': '10.200.50.1',
+    'username': 'cisco',
+    'password': 'cisco'
+}
